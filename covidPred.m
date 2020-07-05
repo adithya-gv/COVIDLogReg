98dd62fed7a27198b598161c48d48cd31e5667ce
Adithya Vasudev <adithya.gvasudev@gmail.com>
2020-07-04 19:24:20 -0700

    Inflection Point Estimator Algorithm added

--- covidPred.m
+++ covidPred.m
@@ -1,40 +1,69 @@
+disp("Loading Data...");
 theta = [0; 0];
 load day.dat;
 X = day;
 load CACaseCount.dat;
 y = CACaseCount;
-ind = size(y, 1);
-max = y(ind);
-diff1 = y(ind) - y(ind - 1);
-diff2 = y(ind - 1) - y(ind - 2);
-diff3 = y(ind - 2) - y(ind - 3);
-ratio1 = diff1/diff2;
-ratio2 = diff2/diff1;
-diff = abs(ratio2-ratio1) / 20;
 iters = 0;
-ratio = (ratio1 + ratio2) / 2;
-while(ratio > 1),
-    ratio = ratio - (diff);
-    max = max + (diff1 * ratio);
-    iters = iters + 1;
+ind = size(y, 1);
+difY = zeros(ind - 1, 1);
+for i = 2:ind,
+    difY(i - 1) = y(i) - y(i-1);
 end;
-max = 2 * max;
-y = y ./ max;
-
-X(:, 2) = X(:, 2) - (ind + iters);
+difY;
+avgY = zeros(ind - 14, 1);
+for i = 14:(ind - 1),
+    avgY(i - 13) = (difY(i - 13) + difY(i - 12) + difY(i - 11) + difY(i - 10) + difY(i - 9) + difY(i - 8) + difY(i - 7) + difY(i - 6) + difY(i - 5) + difY(i - 4) + difY(i - 3) + difY(i - 2) + difY(i - 1) + difY(i)) / 14;
+end;
+avgY;
+maxi = 0;
+indY = 0;
+disp("Estimating Inflection Point...");
+for i = 1:size(avgY),
+    if (y(i) > maxi),
+        maxi = avgY(i);
+        indY = i;
+    end;
+end;
+if (indY == size(avgY, 1)),
+    max = y(ind);
+    diff1 = y(ind) - y(ind - 1);
+    diff2 = y(ind - 1) - y(ind - 2);
+    diff3 = y(ind - 2) - y(ind - 3);
+    ratio1 = diff1/diff2;
+    ratio2 = diff2/diff1;
+    diff = abs(ratio2-ratio1) / 20;
+    ratio = (ratio1 + ratio2) / 2;
+    while(ratio > 1),
+        ratio = ratio - (diff);
+        max = max + (diff1 * ratio);
+        iters = iters + 1;
+    end;
+    max = 2 * max;
+    y = y ./ max;
+    X(:, 2) = X(:, 2) - (ind + iters);
+else,
+    max = 2 * y(indY + 14);
+    X(:, 2) = X(:, 2) - (indY);
+end;
 
 J1 = 1;
 J2 = 0;
-
-disp("Running Logistic Regression (this will take 30-40 seconds).");
-
+status = 0;
+disp("Running Logistic Regression...");
 while ((J1 - J2) > 0.000000001),
+    if (((J1- J2) < 0.0000001) && status == 0),
+        disp("Computing Function...");
+        status = 1;
+    end;
     J1 = costFunction(theta, X, y, ind);
     theta = theta - (0.00003 * thetaUpdate(theta, X, y, ind));
     J2 = costFunction(theta, X, y, ind);
     difference = J1 - J2;
 end;
 theta;
+
+disp("Making Predictions...")
 
 x = [1; (1 - iters)];
 
